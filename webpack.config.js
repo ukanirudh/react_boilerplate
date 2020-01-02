064d8ae850f36992ba40a41eb95f411f911177ae
Anirudh uk <anirudh.kukkilaya@syncron.com>
2020-01-02 13:16:44 +0530

    Anirudh : Adding inline source maps

--- webpack.config.js
+++ webpack.config.js
@@ -1,100 +1,108 @@
-const path = require("path");
-const fs  = require('fs');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "index-bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              plugins: [ '@babel/plugin-transform-runtime' ]
-            }
-          },
-          {
-            loader: 'eslint-loader',
-            options: {
-              emitWarning: true
-            }
-          }
-        ],
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-            loader: 'file-loader',
-            options: {
-                name: '[name].[ext]',
-                outputPath: 'fonts/'
-            }
-        }]
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg|pdf)$/i,
-        use: [{
-            loader: 'file-loader',
-            options: {
-                name: '[name].[ext]',
-                outputPath: 'images/'
-            }
-        }]
-      },
-      // {
-      //   test: /\.css$/,
-      //   use: ["style-loader", "css-loader"]
-      // },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              // only enable hot in development
-              hmr: process.env.NODE_ENV === 'development',
-              // if hmr does not work, this is a forceful method.
-              reloadAll: true,
+module.exports = (env) => {
+  const isDevelopment = env === 'development';
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'index-bundle.js'
+    },
+    devtool: isDevelopment ? 'inline-source-map' : '',
+    devServer: {
+      contentBase: './dist',
+      hot: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                plugins: [ '@babel/plugin-transform-runtime' ]
+              }
             },
+            {
+              loader: 'eslint-loader',
+              options: {
+                emitWarning: true
+              }
+            }
+          ],
+          resolve: {
+            extensions: ['*', '.js']
           },
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.scss$/,
-        use: [
-            "style-loader", // creates style nodes from JS strings
-            "css-loader", // translates CSS into CommonJS
-            "sass-loader" // compiles Sass to CSS, using Node Sass by default
-        ]
-      },
-      {
-        test: /\.less$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          { loader: 'less-loader' }
-        ],
-      },
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [{
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }]
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg|pdf)$/i,
+          use: [{
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+            }
+          }]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                // only enable hot in development
+                hmr: process.env.NODE_ENV === 'development',
+                // if hmr does not work, this is a forceful method.
+                reloadAll: true,
+              },
+            },
+            'css-loader',
+          ],
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader', // creates style nodes from JS strings
+            'css-loader', // translates CSS into CommonJS
+            'sass-loader' // compiles Sass to CSS, using Node Sass by default
+          ]
+        },
+        {
+          test: /\.less$/,
+          use: [
+            { loader: 'style-loader' },
+            { loader: 'css-loader' },
+            { loader: 'less-loader' }
+          ],
+        },
+      ]
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: '[name].css',
+        chunkFilename: '[id].css',
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      })
     ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    })
-  ]
-};
+  };
+}
